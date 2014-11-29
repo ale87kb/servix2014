14493f90c37efc115b9a68c698cfc64e10bd7cdd
horaciom <hmoreno.1@hotmail.com>
2014-11-29 02:14:48 -0300

    vista servicios ofrecidos

--- web/application/views/ofrecer_servicio.php
+++ web/application/views/ofrecer_servicio.php
@@ -54,6 +54,12 @@
                             </div>
                             <div class="col-md-6">
                               <h3>Guia para ofrecer un servicio paso 1</h3>
+                               <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                              </p>
+                              <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                              </p>
                             </div>
                             <div class="col-md-12">
                                 <p class="text-right">
@@ -64,45 +70,92 @@
                           </div>
                           <div role="tabpanel" class="tab-pane" id="paso_2">
                              <div class="col-md-12">
-                              <h2>Completa tus datos de servicio</h2>
+                              <h2>Bien, ahora describe tu servicio </h2>
                             </div>
                             <div class="col-md-6">
                               <div class="form-group">
-                              <label for="titulo">¿Titulo del servicio?</label>
-                              <input type="text" class="form-control" id="titulo" value="" name="titulo"  required="" placeholder="¿Como se llama tu servicio?">
-
+                                <label for="fotoServicio">Sube una foto del servicio que ofreces</label>
+                                <input type="file" id="fotoServicio" name="fotoServicio">
+                              
                               </div>
                               <div class="form-group">
-                              <label for="busqueda-servicio">¿En que categoría lo estas ofreciendo?</label>
-                              <input type="text" class="form-control typeaheadOnlyCat" id="busqueda-servicio" value="" name="categoria" autocomplete="off" required="" placeholder="¿Qué buscas?">
-
-                              </div>
-
-                              <div class="form-group">
-                              <label for="telefono">¿Cual es el numero de teléfono del servicio?</label>
-                              <input type="text" class="form-control" id="telefono" value="" name="telefono"  required="" placeholder="¿A que numero te pueden llamar?">
-
-                              </div>
-                              <div class="form-group">
-                              <label for="sitioweb">¿Sitio web?</label>
-                              <input type="text" class="form-control" id="sitioweb" value="" name="sitioweb"  required="" placeholder="www.misitio.com">
+                                <label for="descripcion">Describe el servicio que ofreces</label>
+                               <textarea class="form-control" id="descripcion" name="descripcion" rows="8"></textarea>
 
                               </div>
 
                             </div>
                             <div class="col-md-6">
-                              <h3>Guia para ofrecer un servicio paso 1</h3>
+                              <h3>Guia para ofrecer un servicio paso 2</h3>
+                               <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                              </p>
+                              <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                              </p>
                             </div>
                             <div class="col-md-12">
-                                <p class="text-right">
-                                    <a href="#" class="btn btn-primary">Ir al paso 2</a>
-                                </p>
+                              
+                                <div class="row">
+                                  <div class="col-md-6">
+                                        <p class="text-left">
+                                            <a href="#" class="btn btn-primary">Ir al paso 1</a>
+                                        </p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p class="text-right">
+                                            <a href="#" class="btn btn-primary">Ir al paso 2</a>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                           </div>
                           <div role="tabpanel" class="tab-pane" id="paso_3">
-                            <div class="col-md-12">
-                              algo 3
-                            </div>
+                                 <div class="col-md-12">
+                                  <h2>Ulimo paso, en que localidad ofreces el servicio </h2>
+                                </div>
+                                <div class="col-md-6">
+                                   <div class="form-group">
+                                      <label for="busqueda-localidad">¿En que localidad lo estas ofreciendo?</label>
+                                    
+                                       <select id="ajax-select" class="form-control selectpicker with-ajax" name="localidad" placeholder="Buscar" data-live-search="true" ></select>
+                                     
+                                    </div>
+                                  <div class="form-group">
+                                    <label for="direccion">En que dirección ofreces el servicio</label>
+                                  <input type="text" class="form-control" id="direccion" value="" name="direccion"  required="" placeholder="Dirección">
+
+                                  </div>
+                                    <div class="form-group">
+                                      <label for="direccion">Marca en el mapa tu dirección</label>
+                                      <img src="http://placehold.it/480x400&text=google map" alt="">
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6">
+                                  <h3>Guia para ofrecer un servicio paso 3</h3>
+                                  <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                                  </p>
+                                  <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ipsum nisi autem at iusto illum excepturi perspiciatis. Praesentium, quod eligendi consectetur officia nemo dolorum quo. Tempora nostrum debitis laudantium porro.
+                                  </p>
+                                </div>
+                                <div class="col-md-12">
+                                  
+                                    <div class="row">
+                                      <div class="col-md-6">
+                                            <p class="text-left">
+                                                <a href="#" class="btn btn-primary">Ir al paso 2</a>
+                                            </p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="text-right">
+                                                <a href="#" class="btn btn-success">Publicar Servicio</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                           </div>
                         </div>
                       </form>
