6159985d04180493e3acd09b467dca6d90a1a523
ale87kb <alejandrodadde@hotmail.com>
2014-12-04 21:19:22 -0300

    estilos
    estilos en el buscador

--- web/application/views/buscador_view.php
+++ web/application/views/buscador_view.php
@@ -5,25 +5,25 @@
         <div class="search-box col-md-12"  >
           <form class="form" role="form" id="formulario-busqueda" action="<?php echo site_url('busqueda');?>" method="POST">
             <div class="row">
-              <div class="col-md-5">
+              <div class="col-md-5 col-xs-12">
                 <div class="form-group input-group-lg">
                   <label class="sr-only" for="busqueda-servicio">¿Que buscas?</label>
                   <input type="text" class="form-control typeaheadCat" id="busqueda-servicio" value="<?php if(isset($servicio)){echo ucfirst($servicio);} ?>" name="servicio" autocomplete="off" required="" placeholder="¿Qué buscas?">
                 </div>
               </div> 
-              <div class="col-md-5">
+              <div class="col-md-5 col-xs-12">
                 <div class="form-group input-group-lg">
                   <label class="sr-only" for="busqueda-localidad">¿Donde lo buscas?</label>
                   <input type="text" class="form-control typeheadLoc2" id="busqueda-localidad" value="<?php if(isset($localidad)){echo ucwords( $localidad);} ?>" name="localidad" autocomplete="off" required="" placeholder="¿Dónde lo buscas?">
                 </div>
               </div>
-              <div class="col-md-2">
+              <div class="col-md-2 col-xs-12">
                 <button type="submit" class="btn btn-success btn-lg btn-block">Buscar</button>
               </div>
             </div>
           </form>
       </div>
-      <div class="col-md-5">
+      <div class="col-md-5 col-xs-12">
         <h3>Solicitá un servicio</h3>
         <small><p>¿No encontrás lo que estás buscando?</p></small>
         <?php 
@@ -41,7 +41,7 @@
         }
         ?>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 col-xs-12">
         <h3>Publicá un servicio</h3>
         <small><p>¿Querés ofrecer un servicio?</p></small>
         <?php 
